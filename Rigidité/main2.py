cabf8eeb1b9589c1d697e3b9a7e3e9d387341091
jsilveira1409 <57415447+jsilveira1409@users.noreply.github.com>
2021-05-18 12:50:48 -0700

    Update main2.py

--- Rigidité/main2.py
+++ Rigidité/main2.py
@@ -12,7 +12,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math as m
-va te pendre pelo
 ###############################################PROGRAM OPTIONS
 graphs = True                   #print graphs ?
 
